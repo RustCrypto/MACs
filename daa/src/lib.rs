e419bfbf420f2d03bd6a395f3e1e000841511a00
Tony Arcieri <bascule@gmail.com>
2020-05-31 07:47:34 -0700

    daa: 2018 edition and `crypto-mac` crate upgrade
    Upgrades to Rust 2018 edition and the (now 2018 edition)
    `crypto-mac` v0.8.0-pre crate.

--- daa/src/lib.rs
+++ daa/src/lib.rs
@@ -6,25 +6,25 @@
 //! # Examples
 //!
 //! ```
-//! use daa::{Daa, Mac};
+//! use daa::{Daa, Mac, NewMac};
 //!
 //! // test from FIPS 113
 //! let key = [0x01, 0x23, 0x45, 0x67, 0x89, 0xAB, 0xCD, 0xEF];
 //! let mut mac = Daa::new_varkey(&key).unwrap();
-//! mac.input(b"7654321 Now is the time for ");
+//! mac.update(b"7654321 Now is the time for ");
 //! let correct = [0xF1, 0xD3, 0x0F, 0x68, 0x49, 0x31, 0x2C, 0xA4];
 //! mac.verify(&correct).unwrap();
 //! ```
+
 #![no_std]
 #![doc(html_logo_url = "https://raw.githubusercontent.com/RustCrypto/meta/master/logo_small.png")]
-pub extern crate crypto_mac;
-extern crate des;
+
+pub use crypto_mac::{self, Mac, NewMac};
 
 use crypto_mac::generic_array::typenum::Unsigned;
 use crypto_mac::generic_array::GenericArray;
-pub use crypto_mac::Mac;
-use crypto_mac::MacResult;
-use des::block_cipher_trait::BlockCipher;
+use crypto_mac::Output;
+use des::block_cipher::{BlockCipher, NewBlockCipher};
 use des::Des;
 
 use core::fmt;
@@ -39,9 +39,8 @@ pub struct Daa {
     pos: usize,
 }
 
-impl Mac for Daa {
-    type OutputSize = <Des as BlockCipher>::BlockSize;
-    type KeySize = <Des as BlockCipher>::KeySize;
+impl NewMac for Daa {
+    type KeySize = <Des as NewBlockCipher>::KeySize;
 
     fn new(key: &GenericArray<u8, Self::KeySize>) -> Self {
         let cipher = Des::new(key);
@@ -51,12 +50,16 @@ impl Mac for Daa {
             pos: 0,
         }
     }
+}
+
+impl Mac for Daa {
+    type OutputSize = <Des as BlockCipher>::BlockSize;
 
     #[inline]
-    fn input(&mut self, mut data: &[u8]) {
+    fn update(&mut self, mut data: &[u8]) {
         let n = <Des as BlockCipher>::BlockSize::to_usize();
-
         let rem = n - self.pos;
+
         if data.len() >= rem {
             let (l, r) = data.split_at(rem);
             data = r;
@@ -93,11 +96,12 @@ impl Mac for Daa {
     }
 
     #[inline]
-    fn result(mut self) -> MacResult<Self::OutputSize> {
+    fn result(mut self) -> Output<Self> {
         if self.pos != 0 {
             self.cipher.encrypt_block(&mut self.buffer);
         }
-        MacResult::new(self.buffer)
+
+        Output::new(self.buffer)
     }
 
     #[inline]
@@ -110,7 +114,7 @@ impl Mac for Daa {
 }
 
 impl fmt::Debug for Daa {
-    fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> Result<(), fmt::Error> {
         write!(f, "Daa")
     }
 }
