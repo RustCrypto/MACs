f10d4c2c9de45c1fe2563f5d4c50c6a470c33730
David Drysdale <dmd@lurklurk.org>
2021-02-27 08:44:53 +0000

    Add Wycheproof HMAC test vectors
    Created from:
    https://github.com/google/wycheproof/blob/2196000605e45d91097147c9c71f26b72af58003/testvectors/
    
    Files:
     - hmac_sha1_test.json
     - hmac_sha256_test.json
     - hmac_sha384_test.json
     - hmac_sha512_test.json

--- hmac/tests/lib.rs
+++ hmac/tests/lib.rs
@@ -4,7 +4,7 @@
 
 #![no_std]
 
-use crypto_mac::new_test;
+use crypto_mac::{new_test, new_trunc_test};
 use hmac::Hmac;
 
 new_test!(hmac_md5, "md5", Hmac<md5::Md5>);
@@ -16,3 +16,20 @@ new_test!(hmac_sha512, "sha512", Hmac<sha2::Sha512>);
 // https://tc26.ru/standard/rs/Р 50.1.113-2016.pdf
 new_test!(hmac_streebog256, "streebog256", Hmac<streebog::Streebog256>);
 new_test!(hmac_streebog512, "streebog512", Hmac<streebog::Streebog512>);
+// Test vectors from Wycheproof; these may include truncated tags.
+new_trunc_test!(hmac_sha1_wycheproof, "wycheproof-sha1", Hmac<sha1::Sha1>);
+new_trunc_test!(
+    hmac_sha256_wycheproof,
+    "wycheproof-sha256",
+    Hmac<sha2::Sha256>
+);
+new_trunc_test!(
+    hmac_sha384_wycheproof,
+    "wycheproof-sha384",
+    Hmac<sha2::Sha384>
+);
+new_trunc_test!(
+    hmac_sha512_wycheproof,
+    "wycheproof-sha512",
+    Hmac<sha2::Sha512>
+);
