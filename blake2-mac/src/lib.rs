8726230bead90d6e9636b5d92fe4e84c7821517e
Артём Павлов <newpavlov@gmail.com>
2021-10-20 20:52:32 +0300

    blake2-mac: add crate attributes

--- blake2-mac/src/lib.rs
+++ blake2-mac/src/lib.rs
@@ -26,6 +26,14 @@
 //! hasher.verify(&code_bytes).unwrap();
 //! ```
 
+#![no_std]
+#![doc(
+    html_logo_url = "https://raw.githubusercontent.com/RustCrypto/meta/master/logo.svg",
+    html_favicon_url = "https://raw.githubusercontent.com/RustCrypto/meta/master/logo.svg"
+)]
+#![forbid(unsafe_code)]
+#![warn(missing_docs, rust_2018_idioms)]
+
 pub use crypto_mac;
 
 use blake2::{
