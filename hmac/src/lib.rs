46797e3b44973a30edb9d7f3a3ebb41810061d90
Artyom Pavlov <newpavlov@gmail.com>
2022-02-17 10:48:57 +0000

    Fix and test min versions build (#108)

--- hmac/src/lib.rs
+++ hmac/src/lib.rs
@@ -79,7 +79,7 @@
 #![doc(
     html_logo_url = "https://raw.githubusercontent.com/RustCrypto/media/26acc39f/logo.svg",
     html_favicon_url = "https://raw.githubusercontent.com/RustCrypto/media/26acc39f/logo.svg",
-    html_root_url = "https://docs.rs/hmac/0.12.0"
+    html_root_url = "https://docs.rs/hmac/0.12.1"
 )]
 #![forbid(unsafe_code)]
 #![cfg_attr(docsrs, feature(doc_cfg))]
