ecaf53430d370155244a81c736f52dae4aeabd4a
Артём Павлов [Artyom Pavlov] <newpavlov@gmail.com>
2022-02-17 13:29:26 +0300

    bump crate versions

--- pmac/src/lib.rs
+++ pmac/src/lib.rs
@@ -43,7 +43,7 @@
 #![doc(
     html_logo_url = "https://raw.githubusercontent.com/RustCrypto/media/26acc39f/logo.svg",
     html_favicon_url = "https://raw.githubusercontent.com/RustCrypto/media/26acc39f/logo.svg",
-    html_root_url = "https://docs.rs/pmac/0.7.0"
+    html_root_url = "https://docs.rs/pmac/0.7.1"
 )]
 #![forbid(unsafe_code)]
 #![cfg_attr(docsrs, feature(doc_cfg))]
