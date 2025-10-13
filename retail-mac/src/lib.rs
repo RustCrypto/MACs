6076fa749de5bd9b0eacfb6d49d4b7e3d8b08769
Артём Павлов [Artyom Pavlov] <newpavlov@gmail.com>
2025-10-13 07:14:07 +0300

    Switch from `doc_auto_cfg` to `doc_cfg`

--- retail-mac/src/lib.rs
+++ retail-mac/src/lib.rs
@@ -4,7 +4,7 @@
     html_logo_url = "https://raw.githubusercontent.com/RustCrypto/media/26acc39f/logo.svg",
     html_favicon_url = "https://raw.githubusercontent.com/RustCrypto/media/26acc39f/logo.svg"
 )]
-#![cfg_attr(docsrs, feature(doc_auto_cfg))]
+#![cfg_attr(docsrs, feature(doc_cfg))]
 #![forbid(unsafe_code)]
 #![warn(missing_docs)]
 
