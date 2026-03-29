d24b49fd233a59fab79f39a9c27cb54f3e63dc66
Артём Павлов [Artyom Pavlov] <newpavlov@gmail.com>
2026-03-30 00:40:52 +0300

    clippy

--- belt-mac/tests/mod.rs
+++ belt-mac/tests/mod.rs
@@ -1,5 +1,5 @@
 //! Test vectors from STB 34.101.31-2020:
-//! https://apmi.bsu.by/assets/files/std/belt-spec371.pdf
+//! <https://apmi.bsu.by/assets/files/std/belt-spec371.pdf>
 
 use belt_mac::BeltMac;
 use digest::dev::reset_mac_test;
--- pmac/tests/mod.rs
+++ pmac/tests/mod.rs
@@ -1,4 +1,4 @@
-//! Test vectors from: http://web.cs.ucdavis.edu/~rogaway/ocb/pmac-test.htm
+//! Test vectors from: <http://web.cs.ucdavis.edu/~rogaway/ocb/pmac-test.htm>
 
 use aes::{Aes128, Aes192, Aes256};
 use digest::{dev::reset_mac_test, new_mac_test};
